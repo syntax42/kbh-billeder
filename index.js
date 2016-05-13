72b4f404c256c24cbbf5691e5a282bdabcc76309
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-05-13 11:12:53 +0200

    Sending a config object to the Collections Online module

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 // This allows loading of environment variables from a .env file
 require('dotenv').config({silent: true});
+var config = require('./config');
 
 var express = require('express');
 var app = express();
 
-var co = require('collections-online');
+var co = require('collections-online')(config);
 
 app.use('/', co);
