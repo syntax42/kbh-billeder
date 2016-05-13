b2aaa45734ab9b582d7b20c90f45e284d5f020ec
Simon Carstensen <me@simonbc.com>
2016-05-13 14:02:37 +0200

    Load tags blacklist from txt file

--- config/env/all.js
+++ config/env/all.js
@@ -3,6 +3,13 @@
 //var path = require('path');
 //var rootPath = path.normalize(__dirname + '/../..');
 
+var path = require('path');
+var fs = require('fs');
+
+const CONFIG_DIR = path.join(__dirname, '..');
+const TAGS_BLACKLIST_PATH = path.join(CONFIG_DIR, 'tags-blacklist.txt');
+var tagsBlacklist = fs.readFileSync(TAGS_BLACKLIST_PATH).toString();
+
 const REVIEW_STATE_FIELD = ''; // TODO: Adjust this to a new field GUID.
 
 module.exports = {
@@ -30,6 +37,7 @@ module.exports = {
   assetFields: require('../asset-fields.json'),
   assetLayout: require('../asset-layout.json'),
   licenseMapping: require('../license-mapping.json'),
+  tagsBlacklist: tagsBlacklist,
   themeColor: '#262626',
   appName: 'KBH Billeder',
 };
