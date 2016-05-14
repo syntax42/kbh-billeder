9e9cb8fec3221aeebad384df107ab973d6f03ccc
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-05-14 09:31:06 +0200

    Adding some comments.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+var express = require('express');
 
 // This allows loading of environment variables from a .env file
 require('dotenv').config({silent: true});
+// Loading the configuration
 var config = require('./config');
 
-var express = require('express');
+// Creating an express app
 var app = express();
 
+// Asking collections online to set-up itself
 var co = require('collections-online')(app, config);
