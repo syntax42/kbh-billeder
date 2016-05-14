6607b8aeaf5c27061a8c1b2a5758305f64fb812f
Kræn Hansen <kraen@socialsquare.dk>
2016-05-15 00:36:17 +0200

    Update staging.sh

--- deployment/staging.sh
+++ deployment/staging.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-$GCLOUD docker push us.gcr.io/${GCLOUD_PROJECT}/frontend
+$GCLOUD docker push eu.gcr.io/${GCLOUD_PROJECT}/frontend
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment kbh-billeder-frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"kbh-billeder-frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
