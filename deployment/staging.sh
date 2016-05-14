88fb38cb18a174fc90b376ccf873d4a5d9ed1df3
Kræn Hansen <kraen@socialsquare.dk>
2016-05-14 23:58:21 +0200

    Update staging.sh

--- deployment/staging.sh
+++ deployment/staging.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-echo "Deploying to the staging server"
-GCLOUD_PROJECT="kbh-billeder"
-# Authenticating following the guide on https://circleci.com/docs/google-auth/
-# Decoding the gcloud service key
-echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
-# Upgrade the gcloud tool
-sudo gcloud --quiet components update
-# Authenticating the gcloud client
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-# Set the gcloud config
-gcloud config set project $GCLOUD_PROJECT
-gcloud config set compute/region europe-west1
-gcloud config set compute/zone europe-west1-d
+
+# Exit on any error
+set -e
+
+$GCLOUD docker push us.gcr.io/${GCLOUD_PROJECT}/frontend
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl patch deployment kbh-billeder-frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"kbh-billeder-frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
