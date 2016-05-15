5b61e10ff038bbe4b6565c9c1c8f7c58bc55967a
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-05-15 02:58:38 +0200

    Changed the name of the deployment

--- deployment/staging.sh
+++ deployment/staging.sh
@@ -5,4 +5,4 @@ set -e
 
 $GCLOUD docker push eu.gcr.io/${GCLOUD_PROJECT}/frontend
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment kbh-billeder-frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"kbh-billeder-frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment kbh-billeder-staging -p '{"spec":{"template":{"spec":{"containers":[{"name":"frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
