ab778c9039e445e2e8f1ffc99ddde99a6e0f59ae
Simon Carstensen <me@simonbc.com>
2016-05-23 09:18:23 +0200

    Add basic Keystone setup

--- server.js
+++ server.js
@@ -10,5 +10,35 @@ var config = require('./config');
 // Creating an express app
 var app = express();
 
+// Set up Keystone
+var keystone = require('keystone');
+
+keystone.init({
+  'name': 'KBH-billeder',
+
+  'static': ['generated'],
+  'views': 'app/views',
+  'view engine': 'jade',
+
+  'auto update': true,
+  'mongo': 'mongodb://localhost/kbh-billeder',
+
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': '1234'
+});
+
+keystone.import('./models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	users: 'users',
+});
+
+keystone.mount('/content', app, function() {
+});
+
 // Asking collections online to set-up itself
 co.initialize(app, config);
