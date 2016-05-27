2589f3935d6b5794a6a8320b806b8ce86fd41e20
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-05-27 09:52:40 +0200

    Removed the gulp modules that are not used

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,7 @@
 // Require - sorted alphabetically after npm name
 // ------------------------------------------
 
-var del = require('del')
-var fs = require('fs')
 var gulp = require('gulp')
-var autoprefixer = require('gulp-autoprefixer')
-var concat = require('gulp-concat')
-var sass = require('gulp-sass')
-var sourcemaps = require('gulp-sourcemaps')
-var svgmin = require('gulp-svgmin')
-var svgstore = require('gulp-svgstore')
-var uglify = require('gulp-uglify')
-var gutil = require('gulp-util')
-var sequence = require('run-sequence')
 
 // ------------------------------------------
 // Get the gulp content from the main
