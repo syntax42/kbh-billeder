b5d33310fb833d5f9b55096c407dfa53245564c2
Andreas Larsen <andreas@larsenwork.com>
2016-06-02 18:22:23 +0200

    more simple page model

--- models/pages.js
+++ models/pages.js
@@ -4,17 +4,14 @@ var keystone = require('keystone'),
 var Page = new keystone.List('Page', {
     autokey: { path: 'slug', from: 'title', unique: true },
     map: { name: 'title' },
-    defaultSort: '-createdAt'
+    defaultSort: 'title'
 });
 
 Page.add({
     title: { type: String, required: true },
-    state: { type: Types.Select, options: 'draft, published, archived', default: 'draft' },
-    author: { type: Types.Relationship, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
-    publishedAt: Date,
-    content: { type: Types.Html, wysiwyg: true, height: 400  }
+    state: { type: Types.Select, options: 'draft, published', default: 'draft' },
+    content: { type: Types.Html, wysiwyg: true, height: 400 }
 });
 
-Page.defaultColumns = 'title, state|20%, author, publishedAt|15%'
+Page.defaultColumns = 'title, state'
 Page.register();
