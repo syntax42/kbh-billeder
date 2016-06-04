f079e027e1af81955d35ad76effc8ee0c39430e3
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-06-04 20:28:28 +0200

    Refreshing menus when the items are saved

--- menus.js
+++ menus.js
@@ -2,23 +2,29 @@ var Q = require('q');
 var keystone = require('keystone');
 
 const MENUS = ['main', 'footer'];
+var app;
 
-module.exports = function(app) {
-  var MenuItem = keystone.list('Menu item');
-
-  var menuItems = {};
-  Q.all(MENUS.map((menu) => {
-  	return MenuItem.model.find({
-  		placement: menu
-  	}).populate('page').exec(function(err, items) {
-      menuItems[menu] = items.map((item) => {
-        item.url = item.getUrl();
-        return item;
+function reload() {
+    var MenuItem = keystone.list('Menu item');
+    var menuItems = {};
+    Q.all(MENUS.map((menu) => {
+    	return MenuItem.model.find({
+    		placement: menu
+    	}).populate('page').exec(function(err, items) {
+        menuItems[menu] = items.map((item) => {
+          item.url = item.getUrl();
+          return item;
+        });
       });
+    })).then(() => {
+      app.set('menus', menuItems);
     });
-  })).then(function() {
-    app.set('menus', menuItems);
-  });
+}
+
+module.exports = function(expressApp) {
+  app = expressApp;
+  reload();
 };
 
+module.exports.reload = reload;
 module.exports.MENUS = MENUS;
