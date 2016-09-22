d70dc28908f14e51f7bc9afc5cb74c01b242293b
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-22 21:41:12 +0200

    Added plugin registration and package initialization

--- start.js
+++ start.js
@@ -1,16 +1,27 @@
-var coPlugins;
-
+var plugins;
 try {
-  coPlugins = require('collections-online/plugins');
+  plugins = require('collections-online/plugins');
 } catch(err) {
   console.error('This module is ment to be run as a plugin for collections online');
   process.exit(1);
 }
 
-coPlugins.register('indexing-engine', require('./indexing/run'));
+module.exports.registerPlugins = () => {
+  plugins.register('indexing-engine', require('./indexing/run'));
+  plugins.register('image-controller', require('./image-controller'));
+  plugins.register('geo-tagging-saver', null);
+};
 
-coPlugins.register('image-controller', require('./image-controller'));
-
-coPlugins.register('geo-tagging-saver', {
-
-});
+// Initialize the cip client and make sure a valid session exists
+module.exports.initialize = (app, config) => {
+  var cip = require('./services/cip');
+  return cip.initSession().then(() => {
+    return require('./cip-categories').initialize(app)
+  }).then(() => {
+    setInterval(() => {
+      // Consider calling close session ..
+      cip.sessionRenew();
+    }, config.cip.sessionRenewalRate || 60*60*1000);
+    console.log('CIP session initialized');
+  });
+};
