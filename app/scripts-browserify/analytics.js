87a88a97822604f818cf4b155c3cbff69bfb1c02
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-10-13 13:00:11 +0200

    Binding event handlers when dom is ready

--- app/scripts-browserify/analytics.js
+++ app/scripts-browserify/analytics.js
@@ -1,16 +1,19 @@
-$('[data-content="asset-download"] .btn').on('click', function(){
-  var href = $(this).attr('href');
-  var hrefArray = href.split("/");
-  var size = hrefArray[hrefArray.length-1];
-  var id = $('.asset').data('id');
-  var catalog = $('.asset').data('catalog');
-  var catalogIdSize = catalog + '-' + id + '-' + size;
-  ga('send', 'event', 'asset', 'download', catalogIdSize);
-  console.log('send', 'event', 'asset', 'download', catalogIdSize);
-});
+$(function() {
+  $('[data-content="asset-download"] .btn').on('click', function(){
+    var href = $(this).attr('href');
+    var hrefArray = href.split("/");
+    var size = hrefArray[hrefArray.length-1];
+    var id = $('.asset').data('id');
+    var catalog = $('.asset').data('catalog');
+    var catalogIdSize = catalog + '-' + id + '-' + size;
+    ga('send', 'event', 'asset', 'download', catalogIdSize);
+    console.log('send', 'event', 'asset', 'download', catalogIdSize);
+  });
 
-$('.search-box').submit(function() {
-  var text = $('.search-box input').val();
-  ga('send', 'event', 'search', 'text', text);
-  console.log('send', 'event', 'search', 'text', text);
+  $('.search-box').submit(function() {
+    var text = $('.search-box input').val();
+    ga('send', 'event', 'search', 'text', text);
+    console.log('send', 'event', 'search', 'text', text);
+  });
+
 });
