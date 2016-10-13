490aef06a279f6bf484989437a921fc6aca27ae1
Andreas Larsen <andreas@larsenwork.com>
2016-10-13 11:21:22 +0200

    console log ga events

--- app/scripts-browserify/analytics.js
+++ app/scripts-browserify/analytics.js
@@ -6,9 +6,11 @@ $('[data-content="asset-download"] .btn').on('click', function(){
   var catalog = $('.asset').data('catalog');
   var catalogIdSize = catalog + '-' + id + '-' + size;
   ga('send', 'event', 'asset', 'download', catalogIdSize);
+  console.log('send', 'event', 'asset', 'download', catalogIdSize);
 });
 
 $('.search-box').submit(function() {
   var text = $('.search-box input').val();
   ga('send', 'event', 'search', 'text', text);
+  console.log('send', 'event', 'search', 'text', text);
 });
