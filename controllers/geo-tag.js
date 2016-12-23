469a34f2b4acab94d3d266239247a7015b26d3ec
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-23 11:39:29 +0100

    Fixing syntax error

--- controllers/geo-tag.js
+++ controllers/geo-tag.js
@@ -3,7 +3,7 @@ const cip = require('../services/cip');
 var GOOGLE_MAPS_COORDS_CROWD_FIELD = '{81780c19-86be-44e6-9eeb-4e63f16d7215}';
 var HEADING_FIELD = '{ef236a08-62f8-485f-b232-9771792d29ba}';
 
-module.exports.updateIndex = {
+module.exports.updateIndex = (metadata) => {
   throw new Error('Not yet implemented: Implement this in your own plugin');
 };
 
