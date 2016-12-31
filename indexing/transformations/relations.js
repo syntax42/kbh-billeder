11028cf519ca1a90d4a3387612dc4dd0d807039b
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-31 04:11:23 +0100

    Related assets are now in an object of related.assets

--- indexing/transformations/relations.js
+++ indexing/transformations/relations.js
@@ -1,6 +1,6 @@
 'use strict';
-
-var cip = require('../../services/cip');
+const _ = require('lodash');
+const cip = require('../../services/cip');
 
 function relatedFilenameComparison(assetA, assetB) {
   var filenameA = assetA.filename;
@@ -9,13 +9,29 @@ function relatedFilenameComparison(assetA, assetB) {
 }
 
 module.exports = function(state, metadata) {
+  metadata.related = {};
   // Transforms the binary representations of each relation.
-  metadata.related_master_assets =
-    cip.parseBinaryRelations(metadata.related_master_assets);
-  metadata.related_sub_assets =
-    cip.parseBinaryRelations(metadata.related_sub_assets);
-  // Sort these by their filename.
-  metadata.related_master_assets.sort(relatedFilenameComparison);
-  metadata.related_sub_assets.sort(relatedFilenameComparison);
+  const masterAssets = cip.parseBinaryRelations(metadata.related_master_assets);
+  const subAssets = cip.parseBinaryRelations(metadata.related_sub_assets);
+  delete metadata.related_master_assets;
+  delete metadata.related_sub_assets;
+
+  // Sort these by their filenames and specify a direction
+  masterAssets.sort(relatedFilenameComparison);
+  masterAssets.forEach(asset => asset.direction = 'parent');
+
+  subAssets.sort(relatedFilenameComparison);
+  subAssets.forEach(asset => asset.direction = 'child');
+
+  metadata.related.assets = _.concat(
+    masterAssets,
+    subAssets
+  );
+  metadata.related.assets.forEach(asset => {
+    // Prepend the collection on the related asset id
+    if(typeof(asset.id) === 'number') {
+      asset.id = metadata.collection + '-' + asset.id;
+    }
+  });
   return metadata;
 };
