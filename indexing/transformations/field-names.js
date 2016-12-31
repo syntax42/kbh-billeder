4ab290d97118cfe216a2e1b881c8bca9cbe87a8a
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-31 04:10:58 +0100

    Derive a "collection" with the same value as "catalog"

--- indexing/transformations/field-names.js
+++ indexing/transformations/field-names.js
@@ -6,5 +6,6 @@ module.exports = function(state, metadata) {
   var transformedMetadata = assetMapping.transform(metadata);
   // The catalog will be removed when formatting.
   transformedMetadata.catalog = metadata.catalog;
+  transformedMetadata.collection = metadata.catalog;
   return transformedMetadata;
 };
