d1313c20efd4015c57deb78ea33a0d26995af6b4
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-01-09 19:30:11 +0100

    Added routing to the /index/asset

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
-module.exports = function (app) {
+const index = require('collections-online-cumulus/controllers/index');
 
+module.exports = function (app) {
+  app.post('/index/asset', index.asset);
 };
