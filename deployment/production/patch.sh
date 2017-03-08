8eeba6d362d06d12830024037fa0290bfeab95e5
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-08 16:55:37 +0100

    Removed the deployment patch template again

--- deployment/production/patch.sh
+++ deployment/production/patch.sh
@@ -8,5 +8,3 @@ IMAGE_PATH="eu.gcr.io/${GCLOUD_PROJECT}/frontend:$CIRCLE_BRANCH-latest"
 # Push the frontend images to
 $GCLOUD docker -- push $IMAGE_PATH
 
-ktmpl -p GCLOUD_PROJECT $GCLOUD_PROJECT -p IMAGE_PATH $IMAGE_PATH
-# kubectl patch deployment frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
