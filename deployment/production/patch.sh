cb2216a512c44e9fcb5e0c2a4200727a3531accc
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-08 16:57:17 +0100

    Patch the deployment in the correct namespace

--- deployment/production/patch.sh
+++ deployment/production/patch.sh
@@ -3,8 +3,11 @@
 # Exit on any error
 set -e
 
-IMAGE_PATH="eu.gcr.io/${GCLOUD_PROJECT}/frontend:$CIRCLE_BRANCH-latest"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGE_PATH="eu.gcr.io/${GCLOUD_PROJECT}/frontend:${CIRCLE_BRANCH}-latest"
+NAMESPACE="production"
 
 # Push the frontend images to
 $GCLOUD docker -- push $IMAGE_PATH
-
+# Patch the frontend deployment, in the correct namespace
+kubectl patch deployment frontend -n $NAMESPACE -p '{"spec":{"template":{"spec":{"containers":[{"name":"frontend","image":"'$IMAGE_PATH'"}]}}}}'
