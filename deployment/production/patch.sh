6ccc62abe54af54f81589641bd83e5231a54b5b8
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-08 16:02:24 +0100

    Push the correct image

--- deployment/production/patch.sh
+++ deployment/production/patch.sh
@@ -6,7 +6,7 @@ set -e
 IMAGE_PATH="eu.gcr.io/${GCLOUD_PROJECT}/frontend:$CIRCLE_BRANCH-latest"
 
 # Push the frontend images to
-$GCLOUD docker -- push $DOCKER_IMAGE
+$GCLOUD docker -- push $IMAGE_PATH
 
 ktmpl -p GCLOUD_PROJECT $GCLOUD_PROJECT -p IMAGE_PATH $IMAGE_PATH
 # kubectl patch deployment frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"frontend","image":"eu.gcr.io/'"$GCLOUD_PROJECT"'/frontend:'"$CIRCLE_SHA1"'"}]}}}}'
