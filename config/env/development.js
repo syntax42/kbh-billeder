94e512cabfeffab19172886885c0851240fa6b13
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-12 14:05:48 +0100

    Using the HTTPs endpoint in development

--- config/env/development.js
+++ config/env/development.js
@@ -7,6 +7,13 @@ module.exports = _.merge(base, {
   env: 'development',
   appName: 'kbhbilleder.dk (dev)',
   allowRobots: true,
+  cip: {
+    baseURL: 'https://www.neaonline.dk:8443/CIP',
+    client: {
+      endpoint: 'https://www.neaonline.dk:8443/CIP/',
+      trustSelfSigned: true
+    }
+  },
   es: {
     log: 'error' //use 'trace' for verbose mode
   },
