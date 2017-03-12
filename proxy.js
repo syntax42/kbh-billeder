27d3a3f887a367b4dbf4becc03b6e52e42c06756
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-12 14:54:06 +0100

    Enabled the use of HTTPs when communicating with the CIP

--- proxy.js
+++ proxy.js
@@ -1,11 +1,22 @@
 
 var config = require('collections-online/lib/config');
 var cip = require('./services/cip');
+
 var Agent = require('agentkeepalive');
-var request = require('request').defaults({
-  agent: new Agent({
+let agent;
+// Choose a keep-alive agent based on the beginning of the baseUrl
+if(config.cip.baseURL.indexOf('https://') === 0) {
+  agent = new Agent.HttpsAgent({
     maxSockets: config.cip.proxyMaxSockets
-  }),
+  });
+} else {
+  agent = new Agent({
+    maxSockets: config.cip.proxyMaxSockets
+  });
+}
+
+var request = require('request').defaults({
+  agent,
   timeout: config.cip.timeout || 55000 // 55 secs
 });
 
@@ -28,7 +39,10 @@ function proxy(url) {
   console.log('CIP proxy is requesting', url);
 
   return request
-    .get(url)
+    .get({
+      url,
+      rejectUnauthorized: config.cip.client.trustSelfSigned ? false : true
+    })
     .on('response', function(response) {
       response.headers['Cache-Control'] = 'max-age=2592000';
     });
