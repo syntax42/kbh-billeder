4a72ab2b724926d1cb02ce5789454ffdeeb684f3
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-12 16:28:21 +0100

    Using the SSL enforced connection to the CIP in production

--- config/env/production.js
+++ config/env/production.js
@@ -7,6 +7,13 @@ module.exports = _.merge(base, {
   env: 'production',
   allowRobots: true,
   appName: 'kbhbilleder.dk',
+  cip: {
+    baseURL: 'https://www.neaonline.dk:8443/CIP',
+    client: {
+      endpoint: 'https://www.neaonline.dk:8443/CIP/',
+      trustSelfSigned: true
+    }
+  },
   googleAnalyticsPropertyID: 'UA-78446616-1',
   host: 'kbhbilleder.dk',
   ip: null,
