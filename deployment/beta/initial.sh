a37ca3d2c9e896bf6600fa168835508958789c74
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-15 13:07:47 +0100

    Deployed the site on beta

---
+++ deployment/beta/initial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAMESPACE="beta"
+ES_DISK_NAME="es-beta-disk"
+MONGO_DISK_NAME="mongo-beta-disk"
+FRONTEND_PUBLIC_IP="35.187.19.75"
+FRONTEND_PUBLIC_HOSTNAME="beta.kbhbilleder.dk"
+
+source $DIR/../initial.sh
