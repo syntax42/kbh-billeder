9b533d1ee3046c7cd8cb6ebdd70d179216bd8719
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-15 13:27:34 +0100

    Created a script that can patch the package.json before installing

---
+++ deployment/package-json-patcher.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const ORIGINAL_PATH = path.join(__dirname, '..', 'package.json');
+assert.ok(fs.existsSync(ORIGINAL_PATH), 'Missing ' + ORIGINAL_PATH);
+const original = require(ORIGINAL_PATH);
+
+const ENV = process.env.NODE_ENV;
+
+if(!ENV) {
+  console.log('Ignoring the', path.basename(__filename), '(no NODE_ENV)');
+} else {
+  console.log('Merging in', ENV, 'package.json into the project');
+  const specializedPath = path.join(__dirname, ENV, 'package.json');
+  assert.ok(fs.existsSync(specializedPath), 'Missing ' + specializedPath);
+
+  const specialized = require(specializedPath);
+  // Set the merge object to the specialized and override with original
+  const merged = Object.assign({}, original);
+  // Merge in the specialized dependencies
+  Object.assign(merged.dependencies, specialized.dependencies);
+  // Merge in the specialized devDependencies
+  Object.assign(merged.devDependencies, specialized.devDependencies);
+  // Write the new package.json to the original file path
+  fs.writeFileSync(ORIGINAL_PATH, JSON.stringify(merged, null, 2) + '\n');
+}
