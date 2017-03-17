c4e001609d1f72940da6a1e08aac694c5daf8cf4
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-17 22:16:13 +0100

    Moved throttling to the services and added some more descriptive logging

--- indexing/processing/result.js
+++ indexing/processing/result.js
@@ -11,18 +11,6 @@ var config = require('collections-online/lib/config');
 var processAsset = require('./asset');
 
 var ASSETS_PER_REQUEST = 100;
-var VISION_ASSETS_PER_REQUEST = 25;
-var VISION_TAGS_PER_100s = 10000;
-var AVG_TAG_CHARACTERS_PER_ASSET = 30;
-var VISION_THROTTLE_SPEED = Math.ceil(100 / (VISION_TAGS_PER_100s /
-                                             (VISION_ASSETS_PER_REQUEST *
-                                              AVG_TAG_CHARACTERS_PER_ASSET)));
-
-function assetsPerRequest(state) {
-  var isIndexVision = state.indexVisionTags || state.indexVisionTagsForce;
-  return (isIndexVision ? VISION_ASSETS_PER_REQUEST : ASSETS_PER_REQUEST);
-}
-
 
 function getResultPage(state, result, pointer, rowCount) {
   var options = {
@@ -60,7 +48,7 @@ function getResultPage(state, result, pointer, rowCount) {
 function processResultPage(state, result, pageIndex) {
   var catalog = result.catalog;
 
-  var totalPages = Math.ceil(result.total_rows / assetsPerRequest(state));
+  var totalPages = Math.ceil(result.total_rows / ASSETS_PER_REQUEST);
   console.log('Queuing page number',
               pageIndex + 1,
               'of',
@@ -71,8 +59,8 @@ function processResultPage(state, result, pageIndex) {
 
   return getResultPage(state,
                        result,
-                       pageIndex * assetsPerRequest(state),
-                       assetsPerRequest(state))
+                       pageIndex * ASSETS_PER_REQUEST,
+                       ASSETS_PER_REQUEST)
   .then(function(assetsOnPage) {
     console.log('Got metadata of page',
                 pageIndex + 1,
@@ -100,35 +88,14 @@ function processNextResultPage(state, result, indexedAssetsIds) {
   var startTime = Date.now();
 
   // Do we still have pages in the result?
-  if (result.pageIndex * assetsPerRequest(state) < result.total_rows) {
+  if (result.pageIndex * ASSETS_PER_REQUEST < result.total_rows) {
     return processResultPage(state, result, result.pageIndex)
     .then(function(newIndexedAssetIds) {
       // Increment the page index of this catalog
       result.pageIndex++;
       // Let's concat the newly index asset ids.
       indexedAssetsIds = indexedAssetsIds.concat(newIndexedAssetIds);
-      return new Promise(function(resolve, reject) {
-
-        if (state.indexVisionTagsPauseCounter >= assetsPerRequest(state)) {
-          state.indexVisionTagsPauseCounter -= assetsPerRequest(state);
-          var waitTime = VISION_THROTTLE_SPEED * 1000 -
-                          (Date.now() - startTime);
-          console.log('Throttle to '+ VISION_THROTTLE_SPEED +
-                      ' seconds so we don\'t overload vision',
-                      waitTime);
-          if (waitTime < 0) {
-            resolve();
-          } else {
-            setTimeout(function() { resolve(); }, waitTime);
-          }
-
-        } else {
-          resolve();
-        }
-
-      }).then(function() {
-        return processNextResultPage(state, result, indexedAssetsIds);
-      });
+      return processNextResultPage(state, result, indexedAssetsIds);
     });
   } else {
     // No more pages in the result, let's return the final array of indexed
--- indexing/transformations/vision-tags.js
+++ indexing/transformations/vision-tags.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var union = require('lodash/union');
-var config = require('collections-online/lib/config');
-var cip = require('../../services/cip');
-var helpers = require('collections-online/shared/helpers');
+const union = require('lodash/union');
+const config = require('collections-online/lib/config');
+const cip = require('../../services/cip');
+const helpers = require('collections-online/shared/helpers');
 
 const plugins = require('collections-online/plugins');
 const motifTagController = plugins.getFirst('motif-tag-controller');
@@ -16,9 +16,15 @@ module.exports = function(state, metadata) {
   // explicitly told. As in run only if one of the indexVison args
   // are specified.
   var runForced = state.indexVisionTagsForce;
-  var runDefault = state.indexVisionTags && !metadata.tags_vision;
+  var runDefault = state.indexVisionTags;
 
-  if ((runForced || runDefault)) {
+  if (runDefault || runForced) {
+    if(metadata.tags_vision && !runForced) {
+      console.log('Asset has vision tags, skipping it');
+      return metadata;
+    } else if (metadata.tags_vision) {
+      console.log('Asset has vision tags, but we´re forced');
+    }
     // increment the counter so we can keep track on when to pause and slow down
     state.indexVisionTagsPauseCounter++;
 
@@ -32,30 +38,29 @@ module.exports = function(state, metadata) {
     return motif.fetchSuggestions(url, state.indexVisionTagsAPIFilter)
       .then(function(tags) {
         // Convert tags to a comma seperated string
-        // Save the tags to Cumulus
-        var tagsIsArray = !!metadata.tags_vision &&
-          typeof metadata.tags_vision === 'object' &&
-          metadata.tags_vision.constructor === Array;
-
-        if (tagsIsArray === false) {
+        if (!Array.isArray(metadata.tags_vision)) {
           metadata.tags_vision = [];
         }
-        var oldTagsSize = metadata.tags_vision.length;
-        var tagsUnion = union(metadata.tags_vision, tags);
-        var diffSize = tagsUnion.length - oldTagsSize;
+        // Hang on to the tags prior to this run
+        const tagsBefore = metadata.tags_vision;
+        // Override the tags with the new suggestions
+        metadata.tags_vision = union(metadata.tags_vision, tags);
+        // Calculate the numberOfNewTags
+        const numberOfNewTags = metadata.tags_vision.length - tagsBefore.length;
 
         // If no new tags was added, we don't save
-        if (diffSize === 0) {
-          console.log('No new tags found, let´s not save back to the CIP');
+        if (numberOfNewTags === 0) {
+          console.log('No new tags found, let´s not save to the CIP.',
+                      'Now the asset has', metadata.tags_vision.length, 'tags');
           return metadata;
         } else {
-          console.log('Derived', diffSize, 'tags, using AI');
-          return motifTagController.save(metadata, tagsUnion)
+          console.log('Derived', numberOfNewTags, 'new tags.',
+                      'Now the asset has', metadata.tags_vision.length, 'tags');
+          return motifTagController.save(metadata, metadata.tags_vision)
           .then(function(response) {
             if (response.statusCode !== 200) {
               throw new Error('Failed to set the field values');
             }
-            metadata.tags_vision = tagsUnion;
             return metadata;
           });
         }
