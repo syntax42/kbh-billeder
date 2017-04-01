34ae8af35d8bf253faa1ca3b7229963af9727dc9
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-01 17:28:22 +0200

    Building up a constant instead of mutating module.exports

--- controllers/motif-tagging.js
+++ controllers/motif-tagging.js
@@ -10,54 +10,56 @@ if(config.features.motifTagging) {
   assert.ok(VISION_FIELD, 'Missing a config.motifTagging.visionField');
 }
 
-module.exports.save = ({id, collection, userTags, visionTags}) => {
-  assert.ok(collection, 'Missing the collection');
-  assert.ok(id, 'Missing an asset id');
+const motifTagging = {
+  save: ({id, collection, userTags, visionTags}) => {
+    assert.ok(collection, 'Missing the collection');
+    assert.ok(id, 'Missing an asset id');
 
-  // Save it using the CIP
-  var values = {};
+    // Save it using the CIP
+    var values = {};
 
-  if(Array.isArray(userTags)) {
-    values[USER_FIELD] = userTags.join(',');
-  }
-
-  if(Array.isArray(visionTags)) {
-    values[VISION_FIELD] = visionTags.join(',');
-  }
-
-  // Set the field values via the CIP
-  return cip.setFieldValues(collection, id, values)
-  .then(function(response) {
-    if (response.statusCode !== 200) {
-      throw new Error('Failed to set the field values');
-    } else {
-      return response;
+    if(Array.isArray(userTags)) {
+      values[USER_FIELD] = userTags.join(',');
     }
-  });
-};
 
-module.exports.updateIndex = (metadata) => {
-  const es = require('collections-online/lib/services/elasticsearch');
-  const index = config.es && config.es.index;
-  assert.ok(index, 'Missing config.es.index');
+    if(Array.isArray(visionTags)) {
+      values[VISION_FIELD] = visionTags.join(',');
+    }
 
-  return es.getSource({
-    id: metadata.collection + '-' + metadata.id,
-    type: 'asset',
-    index
-  }).then(metadataBefore => {
-    const indexingState = {
-      es,
-      index
-    };
-    const transformations = [
-      require('../indexing/transformations/tag-hierarchy')
-    ];
-    const indexAsset = require('../indexing/processing/asset');
-    const metadataAfter = Object.assign({}, metadataBefore, metadata);
-    return indexAsset(indexingState, metadataAfter, transformations)
-    .then(id => {
-      return metadataAfter;
+    // Set the field values via the CIP
+    return cip.setFieldValues(collection, id, values)
+    .then(function(response) {
+      if (response.statusCode !== 200) {
+        throw new Error('Failed to set the field values');
+      } else {
+        return response;
+      }
     });
-  });
+  },
+  updateIndex: metadata => {
+    const es = require('collections-online/lib/services/elasticsearch');
+    const index = config.es && config.es.index;
+    assert.ok(index, 'Missing config.es.index');
+
+    return es.getSource({
+      id: metadata.collection + '-' + metadata.id,
+      type: 'asset',
+      index
+    }).then(metadataBefore => {
+      const indexingState = {
+        es, index
+      };
+      const transformations = [
+        require('../indexing/transformations/tag-hierarchy')
+      ];
+      const indexAsset = require('../indexing/processing/asset');
+      const metadataAfter = Object.assign({}, metadataBefore, metadata);
+      return indexAsset(indexingState, metadataAfter, transformations)
+      .then(id => {
+        return metadataAfter;
+      });
+    });
+  }
 };
+
+module.exports = motifTagging;
