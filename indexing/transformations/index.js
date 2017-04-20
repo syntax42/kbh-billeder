4f2d0d4e838dac322d0f95c10d9e0ea13e693d83
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-20 14:48:18 +0200

    Removed categories transformations

--- indexing/transformations/index.js
+++ indexing/transformations/index.js
@@ -3,13 +3,11 @@ module.exports = [
   require('./empty-title'),
   require('./dates'),
   require('./date-intervals'),
-  require('./categories-and-suggest'),
   require('./relations'),
   require('./dimensions'),
   require('./geo-code'),
   require('./latitude-longitude'),
   require('./split-tags'),
-  require('./category-tags'),
   require('./vision-tags'),
   require('./tag-hierarchy')
 ];
