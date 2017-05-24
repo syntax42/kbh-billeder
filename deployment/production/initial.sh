726dd5855a842fe6e632ac2dbd68524ed9369fd7
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-05-24 13:35:07 +0200

    Added frontend branch to the initial deployment

--- deployment/production/initial.sh
+++ deployment/production/initial.sh
@@ -9,5 +9,6 @@ ES_DISK_NAME="es-production-disk"
 MONGO_DISK_NAME="mongo-production-disk"
 FRONTEND_PUBLIC_IP="146.148.25.25"
 FRONTEND_PUBLIC_HOSTNAME="kbhbilleder.dk"
+BRANCH="production"
 
 source $DIR/../initial.sh
