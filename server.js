c6c9ecd2ab89c0fda8b667af1dcf72a43ddd8fce
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-06-13 13:27:14 +0200

    Made the elasticsearch connection fault resilient

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const Q = require('q');
 const plugins = require('./plugins');
 const config = require('./lib/config');
+const promiseRetry = require('promise-retry');
 
 let co = {
   config: (customizationPath) => {
@@ -49,21 +50,25 @@ let co = {
       const indecies = Object.keys(config.types).map((type) => {
         return config.types[type].index;
       });
-      return ds.count({
-        index: indecies,
-        body: {
-          query: config.search.baseQuery
-        }
-      }).then(function(response) {
-        console.log('Index exists and has', response.count, 'documents.');
-      }, function(err) {
-        if(err.status === 404) {
-          console.error('Missing document index:', indecies.join(' or '));
-        } else {
+      return promiseRetry(retry => {
+        return ds.count({
+          index: indecies,
+          body: {
+            query: config.search.baseQuery
+          }
+        })
+        .catch(( err ) => {
           console.error('Could not connect to the Elasticsearch:',
                         'Is the elasticsearch service started?');
-          process.exit(1);
-        }
+          console.error('Retrying elasticsearch');
+          retry(err);
+        });
+      }, {minTimeout: 4000})
+      .then(response => {
+        console.log('Index exists and has', response.count, 'documents.');
+      }, err => {
+        console.log('Elasticsearch not found after several attempts.');
+        process.exit(1);
       })
       .then(() => {
         console.log('Starting up the server');
