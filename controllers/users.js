667620837f92361047bb59d3d230b54999e904c4
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-09-04 20:29:16 +0200

    Kbhbilleders user controller now serves the profile page with stats and points

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const users = require('collections-online/lib/controllers/users');
+const kbhStatsApi = require('../services/kbh-billeder-stats-api');
+
+users.renderProfile = async (req, res) => {
+  const { user } = req;
+
+  let points = kbhStatsApi.userPoints(user.id);
+  let stats = kbhStatsApi.userStats(user.id);
+
+  try {
+    [points, stats] = [await points, await stats]
+    console.log(points, stats)
+  } catch(err) {
+    console.log(err)
+  }
+
+  res.render('profile', {points , stats, user})
+}
+
+module.exports = users;
