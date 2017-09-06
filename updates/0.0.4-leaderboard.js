2683021016e36c1723b23fabfb819231ff09964b
Mads H. Danquah <mads@danquah.dk>
2017-09-06 18:54:34 +0200

    Create new leaderboard page and add it to the menu
    #150433076

---
+++ updates/0.0.4-leaderboard.js
@@ -0,0 +1,37 @@
+'use strict';
+var Q = require('q');
+var _ = require('lodash');
+var keystone = require('collections-online/plugins/keystone').module;
+
+var Page = keystone.list('Page');
+var MenuItem = keystone.list('Menu item');
+
+module.exports = function(done) {
+  var pages = [
+    new Page.model({
+      title: 'Top 10',
+      slug: 'top',
+      state: 'published',
+      content: '<p>Tekst før statistik</p>\n{{STAT}}\n<p>Tekst efter statistik</p>'
+    }).save()
+  ];
+
+  Q.all(_.values(pages)).then(function(createPages) {
+    pages = {};
+    createPages.forEach((page) => {
+      pages[page.slug] = page;
+    });
+
+    var menuItems = [
+      new MenuItem.model({
+        title: 'Top 10',
+        page: pages['top-10']._id,
+        placement: 'main',
+        order: 6
+      }).save()
+    ];
+    return Q.all(menuItems);
+  }).then(function() {
+    done();
+  }, console.error);
+};
