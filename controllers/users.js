7a900f8260c8f2f4579cd597546980ed05566506
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-09-06 15:57:01 +0200

    Add auth0 management service to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,13 @@
 const users = require('collections-online/lib/controllers/users');
 const kbhStatsApi = require('../services/kbh-billeder-stats-api');
+const auth0 = require('collections-online/lib/services/auth0');
+
+const Service = auth0.Service
 
 users.renderProfile = async (req, res) => {
   const { user } = req;
+
+  console.log(await Service)
 
   let points = kbhStatsApi.userPoints(user.id);
   let stats = kbhStatsApi.userStats(user.id);
