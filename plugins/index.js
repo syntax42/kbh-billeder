45d02761ebc51ebceb93380b71a60c50b0215060
Mads H. Danquah <mads@danquah.dk>
2017-09-06 19:16:29 +0200

    Add a pre-render for rendering statistics from kbhapi
    #150433076

--- plugins/index.js
+++ plugins/index.js
@@ -5,6 +5,7 @@ module.exports.register = () => {
   plugins.register(require('./motif-tagging'));
   plugins.register(require('./geo-tagging'));
   plugins.register(require('./users'));
+  plugins.register(require('./stats-pre-renderer'));
   require('collections-online-cumulus').registerPlugins();
   require('collections-online').registerPlugins();
 };
