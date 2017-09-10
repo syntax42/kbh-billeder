fee123864878860512e695d1140edfdf7764fabb
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-09-10 23:21:20 +0200

    Added appearance option to galleries - either grid or carousel

--- plugins/keystone/models/galleries.js
+++ plugins/keystone/models/galleries.js
@@ -12,6 +12,7 @@ Gallery.add({
     state: { type: Types.Select, options: 'draft, published', default: 'draft' },
     order: { type: Types.Number, format: false },
     items: { type: Types.Relationship, ref: 'Gallery item', many: true },
+    appearance: { type: Types.Select, options: 'grid, carousel', default: 'grid'},
     description: { type: Types.Textarea }
 });
 
