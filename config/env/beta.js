7af6338c6538d1961d2c175374dacdfe144436b8
Mads H. Danquah <mads@danquah.dk>
2017-09-18 10:38:32 +0200

    Enforce https on beta

--- config/env/beta.js
+++ config/env/beta.js
@@ -24,6 +24,7 @@ const beta = _.merge({}, base, {
     requireEmailVerification: true
   },
   host: 'beta.kbhbilleder.dk',
+  enforceHttps: true,
   ip: null,
   port: null,
   socketPath: '/tmp/kbh-billeder.sock'
