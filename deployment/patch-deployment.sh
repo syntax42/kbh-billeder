58aef6ed0fbf160569a511fce5602b730ff04ba9
Mads H. Danquah <mads@danquah.dk>
2018-08-23 17:44:31 +0200

    Switch to circle ci 2.0 config
    - Did away with the legacy patch approach and replaced it with kubectl set image
    - Added a bit of documentation to the build script
    
    KB-399

--- deployment/patch-deployment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Push the frontend images to
-$GCLOUD docker -- push eu.gcr.io/${GCLOUD_PROJECT}/frontend
-# Patch the frontend deployment, in the correct namespace
-kubectl patch deployment frontend -n $NAMESPACE -p '{"spec":{"template":{"spec":{"containers":[{"name":"frontend","image":"'$IMAGE_PATH'"}]}}}}'
