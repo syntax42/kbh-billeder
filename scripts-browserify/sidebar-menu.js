8593a01a5243805aa63370ffcd84c0cefd40f7b8
Mathias Larsen <mathias@reload.dk>
2018-09-06 13:39:01 +0200

    Add icon for mobile menu items with children
    KB-385

--- scripts-browserify/sidebar-menu.js
+++ scripts-browserify/sidebar-menu.js
@@ -4,4 +4,5 @@ $('[data-action="toggle-sidebar-menu"], .gray-overlay').click(function() {
 
 $('[data-action="toggle-sidebar-submenu-item"]').click(function() {
   $(this).parent('li').find('.children').toggleClass('hidden');
+  $(this).parent('.sidebar-menu__children').toggleClass('sidebar-menu__children--inactive');
 });
