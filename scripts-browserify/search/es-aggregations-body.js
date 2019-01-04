c483101ec38a2cc1e13a66a4591d34d3ce423ef1
Mads H. Danquah <mads@danquah.dk>
2019-01-04 07:47:57 +0100

    Going in another direction.
    Instead of having two aggregations (single and multifield) that has to be merged
    in a post-process, we combine the two aggregations into a single aggreagation
    which will allow us to skip the post-process as we get the correct counts right
    away.
    
    This and the subsequent commits cleans up after a quick poc version of the
    aggregation.
    
    This commit removes the postprocess functionallity and fully commits to a merged
    approach.
    
    KB-498

--- scripts-browserify/search/es-aggregations-body.js
+++ scripts-browserify/search/es-aggregations-body.js
@@ -59,7 +59,8 @@ function generateDateRanges() {
 }
 
 // Generate a series of filter-buckets for multi-field dateranges.
-function generateRangeRanges (fromField, toField) {
+function generateRanges (singleField, multiFromField, multiToField) {
+  const hasMulti =  multiFromField && multiToField;
   var result = {};
 
   function getFilterEntry(fieldName, fieldValue, operator) {
@@ -80,129 +81,123 @@ function generateRangeRanges (fromField, toField) {
     return filter;
   }
 
-  function getEntry (fromValue, toValue) {
+  function getPeriodFilterEntry (periodFrom, bucketFrom, periodTo,  bucketTo) {
+    let entry = {
+      'bool': {
+        'should': []
+      }
+    };
+
+    if (bucketTo) {
+      entry.bool.should.push({
+        'bool': {
+          'must' : [
+            getFilterEntry(periodFrom, bucketTo, 'lte'),
+            getFilterEntry(periodTo, bucketTo, 'gte')
+          ]
+        }
+      });
+    }
+
+    if (bucketFrom) {
+      entry.bool.should.push({
+        'bool': {
+          'must' : [
+            getFilterEntry(periodTo, bucketFrom, 'gte'),
+            getFilterEntry(periodFrom, bucketFrom, 'lte')
+          ]
+        }
+      });
+    }
+
+    return entry;
+  }
+
+  function getSingleFilterEntry (creation, bucketFrom, bucketTo) {
     let entry = {
       'bool': {
         'must': []
       }
     };
 
-    if (fromValue) {
-      // Greater than or equal to the specified value.
-      entry.bool.must.push(getFilterEntry(fromField, fromValue, 'gte'));
+    if (bucketTo) {
+      entry.bool.must.push(getFilterEntry(creation, bucketTo, 'lte'));
     }
 
-    if (toValue) {
-      // Less than specified value.
-      entry.bool.must.push(getFilterEntry(toField, toValue, 'lt'));
+    if (bucketFrom) {
+      entry.bool.must.push(getFilterEntry(creation, bucketFrom, 'gte'));
     }
 
     return entry;
   }
 
+  let entryTemplate = {
+    'bool': {
+      'should': []
+    }
+  };
+
   // Start of with a openended range for <= CREATION_INTERVAL_FROM.
-  result['0'] = getEntry(false, CREATION_INTERVAL_FROM);
+  result['*-0'] = {
+    'bool': {
+      'should': []
+    }
+  };
+  result['*-0'].bool.should.push(getSingleFilterEntry(singleField,false, CREATION_INTERVAL_FROM));
+  if (hasMulti) {
+    result['*-0'].bool.should.push(getPeriodFilterEntry(multiFromField,false, multiToField, CREATION_INTERVAL_FROM));
+  }
+
 
   // Then setup a interval every 100 years up until 1900.
-  for(var y = CREATION_INTERVAL_FROM; y < 1900; y+= 100) {
-    let from = y;
-    let to = y + 100;
-
-    result[y] = getEntry(from, to);
+  for(var year = CREATION_INTERVAL_FROM; year < 1900; year+= 100) {
+    let fromYear = year;
+    let toYear = year + 100;
+    const key = `${fromYear}-${toYear}`;
+    result[key] = {
+      'bool': {
+        'should': []
+      }
+    };
+    result[key].bool.should.push(getSingleFilterEntry(singleField, fromYear, toYear));
+    if (hasMulti) {
+      result[key].bool.should.push(getPeriodFilterEntry(multiFromField, fromYear, multiToField, toYear));
+    }
   }
 
   // Then every ten years
   var lastYear;
-  for(let y = 1900; y < CREATION_INTERVAL_TO; y+= 10) {
-    let from = y;
-    let to = y + 10;
-    lastYear = to;
-    result[from] = getEntry(from, to);
+  for(let year = 1900; year < CREATION_INTERVAL_TO; year+= 10) {
+    let fromYear = year;
+    let toYear = year + 10;
+    lastYear = toYear;
+
+    const key = `${fromYear}-${toYear}`;
+    result[key] = {
+      'bool': {
+        'should': []
+      }
+    };
+
+    result[key].bool.should.push(getSingleFilterEntry(singleField, fromYear, toYear));
+    if (hasMulti) {
+      result[key].bool.should.push(getPeriodFilterEntry(multiFromField, fromYear, multiToField, toYear));
+    }
   }
 
   // Finish off with another openended range.
-  result[lastYear] = getEntry(lastYear, false);
-  return result;
-}
-
-/**
- * Post-process a range-filter field.
- */
-function postProcessRangeField (field, aggregationResult) {
-  // We've identified that field is of type date-range. Date-range filters can
-  // work with a single date-field (default) and a "multi" date fields
-  // (to/from)
-  // The Aggregation Result will have a
-  // aggregations.<fieldname>_independent.<fieldname> entry pr. single-field
-  // date-range field.
-  // If the filter is also setup with a multi-field range its result can be
-  // found under
-  // aggregations.<fieldname>_independent.<fieldname>_range
-  // We now go and find any multi-field results, and then augment the
-  // corresponding single-field results.
-
-  // Ensure we have all the data we need.
-  if (
-    !aggregationResult['aggregations']
-    || !aggregationResult['aggregations'][field + '_independent']
-    || !aggregationResult['aggregations'][field + '_independent'][field]
-    || !aggregationResult['aggregations'][field + '_independent'][field]['buckets']
-    || !aggregationResult['aggregations'][field + '_independent'][field]['buckets'].length
-    || !aggregationResult['aggregations'][field + '_independent'][`${field}_range`]
-    || !aggregationResult['aggregations'][field + '_independent'][`${field}_range`]['buckets']) {
-    return;
+  result[`${lastYear}-*`] = {
+    'bool': {
+      'should': []
+    }
+  };
+  result[`${lastYear}-*`].bool.should.push(getSingleFilterEntry(singleField, lastYear, false));
+  if (hasMulti) {
+    result[`${lastYear}-*`].bool.should.push(getPeriodFilterEntry(multiFromField, lastYear, multiToField, false));
   }
 
-  // We've verified we have results for the main date-range aggregation and
-  // the multi-field range, now go trough each single-field range and look up
-  // any multi-field range results and augment.
-  const singleFieldRanges = aggregationResult['aggregations'][field + '_independent'][field].buckets;
-  const multiFieldRanges = aggregationResult['aggregations'][field + '_independent'][`${field}_range`].buckets;
-
-  singleFieldRanges.forEach(function(rangeBucket, index) {
-    // Pick out the year for the range.
-    let fromYear;
-    if (rangeBucket['from_as_string']) {
-      fromYear = rangeBucket['from_as_string'];
-    }
-    else {
-      // Allow the initial (open-ended range bucket) not to have a from.
-      if (index === 0) {
-        fromYear = 0;
-      }
-      else {
-        return;
-      }
-    }
-
-    // Add the multi-field counts to the singlefield if found.
-    if (!multiFieldRanges[fromYear]
-      || !multiFieldRanges[fromYear]['doc_count']) {
-      return;
-    }
-    rangeBucket['doc_count'] += multiFieldRanges[fromYear]['doc_count'];
-  });
+  return result;
 }
-
-
-// The post-processing step lets us modify the various filter results.
-module.exports.postProcess = function (aggregationResult) {
-
-  // Go trough the filteres and identify date-ranges.
-  Object.keys(config.search.filters).forEach(function(field) {
-    const filter = config.search.filters[field];
-    if (
-      filter.type === 'date-range' &&
-      filter.multifield &&
-      filter.multifield.from &&
-      filter.multifield.to) {
-      postProcessRangeField(field, aggregationResult);
-    }
-
-  });
-
-  return aggregationResult;
-};
 
 module.exports.generateBody = function(parameters, body) {
   var result = {
@@ -228,19 +223,11 @@ module.exports.generateBody = function(parameters, body) {
       if(!filter.field) {
         throw new Error('Expected "field" option on filter field: ' + field);
       }
-      // Tried the date histogram /w interval: '3650d' // Not really 10 years
-      // See https://github.com/elastic/elasticsearch/issues/8939
-      aggs[field] = {
-        date_range: {
-          field: filter.field,
-          format: 'yyy',
-          ranges: generateDateRanges()
-        }
-      };
+
       if (filter.multifield && filter.multifield.from && filter.multifield.to) {
-        aggs[field + '_range'] = {
+        aggs[field] = {
           filters: {
-            filters: generateRangeRanges(filter.multifield.from, filter.multifield.to)
+            filters: generateRanges(filter.field, filter.multifield.from, filter.multifield.to)
           }
         };
       }
