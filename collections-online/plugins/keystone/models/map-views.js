e26e120290b74e0e789da8ddea2b5ea429e6a9b3
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2020-05-13 19:18:39 +0200

    Add map view and tag cloud models to keystone

---
+++ collections-online/plugins/keystone/models/map-views.js
@@ -0,0 +1,17 @@
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+
+const MapView = new keystone.List('Map view', {
+    autokey: { path: 'slug', from: 'title', unique: true },
+    map: { name: 'title' },
+    defaultSort: 'order'
+});
+
+MapView.add({
+    title: { type: String, required: true },
+    queryString: { type: Types.Text },
+});
+
+MapView.defaultColumns = 'title'
+
+module.exports = MapView;
