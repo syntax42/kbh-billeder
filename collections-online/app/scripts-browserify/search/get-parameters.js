97c94d483215d12045cc81911da38ff026760010
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2020-05-27 14:10:56 +0200

    allow special symbols ?*" in search again (publicly documented lucene symbols)

--- collections-online/app/scripts-browserify/search/get-parameters.js
+++ collections-online/app/scripts-browserify/search/get-parameters.js
@@ -38,9 +38,10 @@ module.exports = function() {
       // Escape all Lucene special characters, to avoid syntax errors in
       // Elastic Search query, c.f.
       // https://lucene.apache.org/core/3_0_3/queryparsersyntax.html#Escaping%20Special%20Characters
+      // BUT with the note that we allow ?, ", and *.
       [
         '\\+', '\\-', '\&\&', '\\|\\|', '!', '\\(', '\\)', '\\{', '\\}',
-        '\\[', '\\]', '\\^', '"', '~', '\\*', '\\?', '\\:', '\\\\'
+        '\\[', '\\]', '\\^', '~', '\\:', '\\\\'
       ].forEach((luceneSpecialCharacter) => {
         const characterGlobalPattern = new RegExp(luceneSpecialCharacter, 'g');
         if(Array.isArray(value)) {
