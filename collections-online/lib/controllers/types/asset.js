ddc8a08e2273186d1050983b19b7df9753e072a8
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-04-13 22:49:39 +0200

    Replace mocked data with real elasticsearch data when getting assets

--- collections-online/lib/controllers/types/asset.js
+++ collections-online/lib/controllers/types/asset.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const ds = require('../../services/documents');
 const documentController = require('../document');
-const config = require('../../config');
+const es = require('../../services/elasticsearch');
 
 const layouts = require('../../layouts');
 const assetSection = layouts.section('asset');
@@ -20,76 +19,27 @@ exports.index = function(req, res, next) {
     });
   })
     .then(function(metadata) {
-      const assetSeries = [
-        {
-          title: "Vesterbrogade",
-          assets: [ "magasin-museum-19870", "kbh-museum-8366" ],
-          previewAssets: [
-            {
-              collection: "magasin-museum",
-              creation_time: { displaystring: "1969", year: 1969, timestamp: "1969-01-01", month: null, day: null },
-              creation_time_estimated: null,
-              creation_time_from: null,
-              creation_time_to: null,
-              description: "Magasin Torv foran forretningen på Kgs. Nytorv får plantet træer. Magasin Torv skal være en skyggefuld oase både for forretningens kunder og byens borgere. ",
-              file_format: "TIFF Image",
-              id: 19870,
-              short_title: "Der plantes træer foran Magasin",
-              tags: ["arbejdsliv", "arbejdsmand", "vejtræ", "striktrøje"]
-            },
-            {
-              collection: "kbh-museum",
-              creation_time: { displaystring: "1969", year: 1969, timestamp: "1969-01-01", month: null, day: null },
-              creation_time_estimated: null,
-              creation_time_from: null,
-              creation_time_to: null,
-              description: "Magasin Torv foran forretningen på Kgs. Nytorv får plantet træer. Magasin Torv skal være en skyggefuld oase både for forretningens kunder og byens borgere. ",
-              file_format: "TIFF Image",
-              id: 8366,
-              short_title: "Der plantes træer foran Magasin",
-              tags: ["arbejdsliv", "arbejdsmand", "vejtræ", "striktrøje"]
+      return es.search({
+        type: 'series',
+        body: {
+          query: {
+            match: {
+              assets: `${metadata.collection}-${metadata.id}`
             }
-          ],
-          description: "Vesterbrogade 67, Carl P. Dreyers Vinhandel med tilhørende vinstue. Th. S. Christensens Frugt & Vildt-forretning. Bygningen nedbrudt efteråret 1902. Billedet stemplet Chr. Neuhaus Eftf. Oluf W. Jørgensen, Kjøbmagergade 14.",
-          date1: { year: 1902 },
-          date2: { year: 1942 },
-          tags: [ "hej", "verden" ],
-        },
-        {
-          title: "Vesterbrogade",
-          assets: [ "magasin-museum-19870", "kbh-museum-8366" ],
-          previewAssets: [
-            {
-              collection: "magasin-museum",
-              creation_time: { displaystring: "1969", year: 1969, timestamp: "1969-01-01", month: null, day: null },
-              creation_time_estimated: null,
-              creation_time_from: null,
-              creation_time_to: null,
-              description: "Magasin Torv foran forretningen på Kgs. Nytorv får plantet træer. Magasin Torv skal være en skyggefuld oase både for forretningens kunder og byens borgere. ",
-              file_format: "TIFF Image",
-              id: 19870,
-              short_title: "Der plantes træer foran Magasin",
-              tags: ["arbejdsliv", "arbejdsmand", "vejtræ", "striktrøje"]
-            },
-            {
-              collection: "kbh-museum",
-              creation_time: { displaystring: "1969", year: 1969, timestamp: "1969-01-01", month: null, day: null },
-              creation_time_estimated: null,
-              creation_time_from: null,
-              creation_time_to: null,
-              description: "Magasin Torv foran forretningen på Kgs. Nytorv får plantet træer. Magasin Torv skal være en skyggefuld oase både for forretningens kunder og byens borgere. ",
-              file_format: "TIFF Image",
-              id: 8366,
-              short_title: "Der plantes træer foran Magasin",
-              tags: ["arbejdsliv", "arbejdsmand", "vejtræ", "striktrøje"]
-            }
-          ],
-          description: "Vesterbrogade 67, Carl P. Dreyers Vinhandel med tilhørende vinstue. Th. S. Christensens Frugt & Vildt-forretning. Bygningen nedbrudt efteråret 1902. Billedet stemplet Chr. Neuhaus Eftf. Oluf W. Jørgensen, Kjøbmagergade 14.",
-          date1: { year: 1902 },
-          date2: { year: 1942 },
-          tags: [ "hej", "verden" ],
+          }
         }
-      ];
+      })
+        .then((seriesSearchResult) => {
+          const assetSeries = seriesSearchResult.hits.hits
+            .map((seriesDoc) => seriesDoc._source);
+
+          return {
+            assetSeries,
+            metadata,
+          };
+        });
+    })
+    .then(({assetSeries, metadata}) => {
       res.render('asset.pug', {
         metadata,
         assetSection: assetSection(),
