5aa5e77cd978f06d29b8ca482cbbdec2268a1654
Philip <philip@deranged.dk>
2021-05-07 11:18:50 +0200

    ensure that if this by mistake is merged into prod, it wont affect it

--- config/env/production.js
+++ config/env/production.js
@@ -30,7 +30,7 @@ const production = _.merge({}, base, {
     analyticsPropertyID: 'UA-78446616-1'
   },
   host: 'kbhbilleder.dk',
-  reindexAccessKey: process.env.REINDEX_ACCESS_KEY_PROD,
+  // reindexAccessKey: process.env.REINDEX_ACCESS_KEY_PROD,
   enforceHttps: true,
   ip: null,
   port: null,
