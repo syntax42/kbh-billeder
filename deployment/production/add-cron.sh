36d682c1208526ade9b1c96a6fd729b1343cb4db
Philip <philip@deranged.dk>
2021-05-19 14:40:17 +0200

    attempt to add access key as an env var to deployment

--- deployment/production/add-cron.sh
+++ deployment/production/add-cron.sh
@@ -6,6 +6,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAMESPACE="production"
 FRONTEND_PUBLIC_HOSTNAME="kbhbilleder.dk"
+KBH_ACCESS_KEY=${KBH_ACCESS_KEY}
 # All hours except at 04
 INCREMENTAL_TIMING="02 02 0,1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *"
 # Full reindex at 02
