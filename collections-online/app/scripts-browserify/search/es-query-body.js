537eb92b4c5b763e749be0816bf2994d381ea4ba
Philip <philip@deranged.dk>
2021-06-08 20:50:13 +0200

    enable search for id's

--- collections-online/app/scripts-browserify/search/es-query-body.js
+++ collections-online/app/scripts-browserify/search/es-query-body.js
@@ -181,7 +181,7 @@ module.exports = function(parameters) {
                 'short_title^5',
                 'description',
                 'previewAssets.short_title^3',
-                // 'id',
+                'id',
                 'previewAssets.description',
                 'previewAssets.tags',
                 'catalog_name',
