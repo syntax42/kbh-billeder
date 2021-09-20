dc5d837e538661aeb4f00653186d8b2bc3e6f1de
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-09-20 14:15:10 +0200

    Inline collections-online/indexing in kbhbilleder/indexing

--- indexing.js
+++ indexing.js
@@ -5,5 +5,19 @@ co.config(__dirname);
 // Register collections-online plugins
 require('./plugins').register();
 
-// Start the indexing
-require('./collections-online/indexing').run();
+const collectionsOnlinePlugins = require('./collections-online/plugins');
+
+function run(state) {
+  // Run the indexing with the first available indexing-engine
+  var indexingEngine = collectionsOnlinePlugins.getFirst('indexing-engine');
+  return indexingEngine(state || {}).then(function() {
+    console.log('\nAll done - good bye!');
+    process.exit(0);
+  }, function(err) {
+    console.error('An error occured!');
+    console.error(err.stack || err);
+    process.exit(1);
+  });
+}
+
+run();
