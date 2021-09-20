c06c96fe6e438f9943daf84dc1da9052fbe14bec
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-09-20 16:18:17 +0200

    Readability fixes in server.js

--- collections-online/server.js
+++ collections-online/server.js
@@ -56,40 +56,36 @@ let co = {
             query: config.search.baseQuery
           }
         })
-        .catch(( err ) => {
-          console.error('Could not connect to the Elasticsearch:',
-                        'Is the elasticsearch service started?');
-          console.error('Retrying elasticsearch');
-          retry(err);
-        });
+          .catch(( err ) => {
+            console.error('Could not connect to the Elasticsearch:', 'Is the elasticsearch service started?');
+            console.error('Retrying elasticsearch');
+            retry(err);
+          });
       }, {minTimeout: 4000})
-      .then(response => {
-        console.log('Index exists and has', response.count, 'documents.');
-      }, err => {
-        console.log('Elasticsearch not found after several attempts.');
-        process.exit(1);
-      })
-      .then(() => {
-        console.log('Starting up the server');
-        // Start server
-        if(config.port && config.ip) {
-          app.listen(config.port, config.ip, function() {
-            console.log('Express server listening on %s:%d, in %s mode',
-                        config.ip, config.port, app.get('env'));
-          });
-        } else if(config.socketPath) {
-          app.listen(config.socketPath, function() {
-            console.log('Express server listening on socket %s, in %s mode',
-                        config.socketPath, app.get('env'));
-          });
-        } else {
-          throw new Error('Could not start server, needed "port" and "ip" ' +
-                          'or "socketPath" in the configuration.');
-        }
-      }, (err) => {
-        console.error('Error when starting the app: ', err.stack);
-        process.exit(2);
-      });
+        .then(response => {
+          console.log('Index exists and has', response.count, 'documents.');
+        }, err => {
+          console.log('Elasticsearch not found after several attempts.');
+          process.exit(1);
+        })
+        .then(() => {
+          console.log('Starting up the server');
+          // Start server
+          if(config.port && config.ip) {
+            app.listen(config.port, config.ip, function() {
+              console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+            });
+          } else if(config.socketPath) {
+            app.listen(config.socketPath, function() {
+              console.log('Express server listening on socket %s, in %s mode', config.socketPath, app.get('env'));
+            });
+          } else {
+            throw new Error('Could not start server, needed "port" and "ip" or "socketPath" in the configuration.');
+          }
+        }, (err) => {
+          console.error('Error when starting the app: ', err.stack);
+          process.exit(2);
+        });
     });
   },
   registerRoutes: app => {
