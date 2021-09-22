4a7cb858b312203c927c275de063b5e1f42a4f79
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-09-22 11:51:29 +0200

    Simplify plugin registering in plugins/index

--- plugins/index.js
+++ plugins/index.js
@@ -1,26 +1,14 @@
 'use strict';
 const plugins = require('../pluginController');
-const config = require('../lib/config');
 
 module.exports.register = () => {
   plugins.register(require('./users'));
-
-  if(config.es) {
-    plugins.register(require('../plugins/elasticsearch'));
-  }
-  if(config.features.keystone) {
-    plugins.register(require('./keystone'));
-  }
-  if(config.features.users) {
-    plugins.register(require('../plugins/auth'));
-  }
-
+  plugins.register(require('./elasticsearch'));
+  plugins.register(require('./keystone'));
+  plugins.register(require('./auth'));
   plugins.register(require('./image'));
   plugins.register(require('./motif-tagging'));
   plugins.register(require('./geo-tagging'));
   plugins.register(require('./stats-pre-renderer'));
-  
-  if(config.es) {
-    plugins.register(require('./indexing'));
-  }
+  plugins.register(require('./indexing'));
 };
