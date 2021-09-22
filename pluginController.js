6061af33f8c670e05da6c6c49a6b8dbcc75f94c3
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2021-09-22 15:21:41 +0200

    Remove unused or unreferenced methods from pluginController

--- pluginController.js
+++ pluginController.js
@@ -5,37 +5,12 @@ const pluginModulesByType = {};
 // An array of plugins
 const registeredPlugins = [];
 
-var requiredMethods = {
-  'image-controller': [
-    'proxy',
-    'proxyDownload',
-    'proxyThumbnail'
-  ]
-};
-
-function validateMethods(type, plugin) {
-  var expectedMethods = requiredMethods[type] || [];
-  expectedMethods.forEach((method) => {
-    if(!plugin[method]) {
-      throw new Error('The image controller is missing a method: ' + method);
-    }
-  });
-}
-
-function validatePlugin(type, plugin) {
-  validateMethods(type, plugin);
-}
-
-exports.validatePlugin = validatePlugin;
-
 exports.register = (plugin) => {
   if(!plugin.type) {
     throw new Error('The plugin was missing a type');
   } else if(!plugin.module) {
     throw new Error('The plugin was missing a module');
   }
-  // Validate the plugin, throwing an error if invalid
-  validatePlugin(plugin.type, plugin.module);
   // Let's notify the developer that the plugin was registered
   console.log('A plugin of type "' + plugin.type + '" was registered');
   // Create an array for the plugins of this type, if it doesn't exist
@@ -45,20 +20,6 @@ exports.register = (plugin) => {
   // Push the plugin to the list
   pluginModulesByType[plugin.type].push(plugin.module);
   registeredPlugins.push(plugin);
-};
-
-exports.getFirst = (type) => {
-  if(pluginModulesByType[type] && pluginModulesByType[type].length > 0) {
-    return pluginModulesByType[type][0];
-  }
-  throw new Error('No plugins of the desired type (' + type + ')');
-};
-
-exports.getAll = (type) => {
-  if(pluginModulesByType[type] && pluginModulesByType[type].length > 0) {
-    return pluginModulesByType[type];
-  }
-  return [];
 };
 
 exports.initialize = (app, config) => {
