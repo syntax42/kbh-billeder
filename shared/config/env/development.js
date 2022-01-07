5e766f596a8f5173d77e86079555f82bd90a1c38
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-01-07 13:18:11 +0100

    Align ordering of config vars between files

--- shared/config/env/development.js
+++ shared/config/env/development.js
@@ -4,11 +4,11 @@ var _ = require('lodash');
 var base = require('./base');
 
 const development = _.merge({}, base, {
-  env: 'development',
   auth0: {
     callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:9000/auth/callback',
     clientID: process.env.AUTH0_CLIENT_ID || 'uyTltKDRg1BKu3nzDu6sLpHS44sInwOu',
   },
+  env: 'development',
   kbhAccessKey: process.env.KBH_ACCESS_KEY,
   siteTitle: 'kbhbilleder.dk (dev)',
   allowRobots: true,
